d24db1e173de04a2fa00c18abdbb4e105535e16c
luis.henrique <luisnozeta@gmail.com>
2026-04-22 16:17:00 -0300

    fix: Conserta bug de travar ao considerar entradas inválidas (issue #14)

--- app/static/js/conversoes/main.js
+++ app/static/js/conversoes/main.js
@@ -14,41 +14,58 @@ const bases_dict = {
 }
 
 /**
+ * Verifica se a entrada enviada pelo usuário é possível conforme a base de origem selecionada.
+ * @param {string} string - Valor digitado pelo usuário.
+ * @param {number} base - Base em que ele digitou o valor.
+ * @returns - Retorna true se valor e base de origem estão de acordo, false se não.
+ */
+function isValid(string, base) {
+    const regexes = {
+        10: /^\d+$/g,
+        2: /^[0-1]+$/g,
+        8: /^[0-7]+$/g,
+        16: /^[0-9a-fA-F]+$/g,
+    }
+
+    return regexes[base].test(string)
+}
+
+/**
  * Converte os valores digitados para outra base numérica e exibe ambos.
  * Além disso, a função também decide, conforme as bases numéricas escolhidas pelo usuário,
  * qual método de conversão exibir, ensinando-o como fazer a conversão a partir da teoria.
  */
 function convert() {    
-    let decimalValue = parseInt(originInput.value, bases_dict[baseOrigin.value])
+    const string = originInput.value.trim()
+    const bOrigin = bases_dict[baseOrigin.value]
+    const bDestiny = bases_dict[baseDestiny.value]
 
-    if (decimalValue < 0) {
-        destinyInput.value = "Sem valores negativos"
-        cleanUpdates()
-    } else if (isNaN(decimalValue)) {
-        destinyInput.value = "Digite um número válido"
-        cleanUpdates()
-    } else if (baseOrigin.value == baseDestiny.value) {
-        destinyInput.value = "Modifique uma das bases"
-        cleanUpdates()
-    } else {
-        let finalValue = decimalValue.toString(bases_dict[baseDestiny.value])
+    cleanUpdates()
+    destinyInput.value = ""
+
+    if (!string) return
+
+    if (bOrigin == bDestiny) {
+        destinyInput.value = "Bases iguais"
+        return
+    }
+
+    if (isValid(string, bOrigin)) {
+        let decimalValue = parseInt(string, bOrigin)
+        let finalValue = decimalValue.toString(bDestiny)
         destinyInput.value = finalValue
 
-        if (baseOrigin.value == "Decimal") {
-            updateSuccessiveDivision(decimalValue, bases_dict[baseDestiny.value])    
-            updatePolinomialNotation()
-            updateBitGrouping()
+        if (bOrigin == 10) {
+            updateSuccessiveDivision(decimalValue, bDestiny)    
         } else {
-            if (baseDestiny.value == "Decimal") {
-                updatePolinomialNotation(decimalValue, bases_dict[baseOrigin.value])
-                updateSuccessiveDivision()
-                updateBitGrouping()
+            if (bDestiny == 10) {
+                updatePolinomialNotation(decimalValue, bOrigin)
             } else{
-                updateBitGrouping(decimalValue, bases_dict[baseOrigin.value], bases_dict[baseDestiny.value])
-                updateSuccessiveDivision()
-                updatePolinomialNotation()
+                updateBitGrouping(decimalValue, bOrigin, bDestiny)
             }
         }
+    } else {
+        destinyInput.value = "Número inválido"
     }
 }
 
